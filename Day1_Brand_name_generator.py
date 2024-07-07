36de0ae674ce39bb9e8ea38abee2df9919481943
HariPriya-hub01 <rharipriya2099@gmail.com>
2024-07-07 21:52:55 +0530

    Add files via upload

---
+++ Day1_Brand_name_generator.py
@@ -0,0 +1,5 @@
+#Generate a brand name using input from user. The user shud type the input in new line
+print("Welcome to the brand name generator.")
+frst_name = input("Enter the city you were born in:\n")
+pet_name = input ("Enter the name of your pet:\n")
+print("Your brand name could be: " + frst_name + " " + pet_name)
